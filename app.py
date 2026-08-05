4c8e876e35bb476b7b77e24d7ffeee9a108f761b
Parthiban-VIT <parthiban.v@vit.ac.in>
2026-08-05 16:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ from login import login_page
 st.set_page_config(
     page_title="Class Monitoring System",
     page_icon="📚",
-    layout="wide"
+    layout="centered"
 )
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-if st.session_state.logged_in:
-
-    st.success(f"Welcome {st.session_state.name}")
-
-else:
-
-    login_page()
+login_page()
