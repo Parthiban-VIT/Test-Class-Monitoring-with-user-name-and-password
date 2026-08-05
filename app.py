2820efcd55e538d55e2c6d52aabf1759abad11b9
Parthiban-VIT <parthiban.v@vit.ac.in>
2026-08-05 16:39:53 +0530

    Change layout to wide and update login logic

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ from login import login_page
 st.set_page_config(
     page_title="Class Monitoring System",
     page_icon="📚",
-    layout="centered"
+    layout="wide"
 )
 
-login_page()
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if st.session_state.logged_in:
+
+    st.success(f"Welcome {st.session_state.name}")
+
+else:
+
+    login_page()
