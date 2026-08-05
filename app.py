229a4f8d7daebe4aed3e960657df9c881235cf57
Parthiban-VIT <parthiban.v@vit.ac.in>
2026-08-05 16:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from login import login_page
 
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+    login_page()
+else:
+    st.write("Welcome!")
